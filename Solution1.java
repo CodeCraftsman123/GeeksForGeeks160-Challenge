e388004f4e24f9d8338f61311c23e74e09e29df6
CodeCraftsman123 <prasadkamble0807@gmail.com>
2025-01-11 08:35:58 +0530

    Done

--- Solution1.java
+++ Solution1.java
@@ -1,8 +1,6 @@
-import java.util.HashSet;
 public class Solution1 {
     public int getSecondLargest(int[] arr) {
         int largest = Integer.MIN_VALUE, secondLargest = Integer.MIN_VALUE;
-        HashSet<Integer>hs = new HashSet<>();
 
         for(int element:arr)
         {
@@ -10,16 +8,14 @@ public class Solution1 {
             {
                 secondLargest = largest;
                 largest = element;
-                hs.add(element);
             }
             else if(element > secondLargest && element!=largest)
             {
                 secondLargest = element;
-                hs.add(element);
             }
         }
 
-        if(hs.size() <= 1)
+        if(secondLargest == Integer.MIN_VALUE)
             return -1;
         return secondLargest ;
     }
