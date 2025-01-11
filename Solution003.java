1949e02b9b38cc7aad87f83f40d969566187eb66
CodeCraftsman123 <prasadkamble0807@gmail.com>
2025-01-11 16:07:49 +0530

    Updated

--- Solution003.java
+++ Solution003.java
@@ -1,4 +1,12 @@
 public class Solution003
 {
-
+    public void reverseArray(int[] arr)
+    {
+        for(int i = 0 ; i < (arr.length/2) ; i++)
+        {
+            int temp = arr[i];
+            arr[i] = arr[arr.length - i - 1];
+            arr[arr.length-i-1] = temp;
+        }
+    }
 }
