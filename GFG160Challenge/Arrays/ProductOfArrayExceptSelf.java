c09c1d2b92ed7113a1cd0e89e0a3078897e58374
CodeCraftsman123 <prasadkamble0807@gmail.com>
2025-01-18 14:10:02 +0530

    Updated

---
+++ GFG160Challenge/Arrays/ProductOfArrayExceptSelf.java
@@ -0,0 +1,25 @@
+package GFG160Challenge.Arrays;
+
+import java.util.Arrays;
+
+public class ProductOfArrayExceptSelf {
+    public int[] productExceptSelf(int[] nums) 
+    {
+        int[] ans = new int[nums.length];
+        int left = 1, right = 1;
+        Arrays.fill(ans,1);
+
+        for(int i = 0 ; i < nums.length;i++)
+        {
+            ans[i] *= left;
+            left *= nums[i];
+        }
+
+        for(int i = nums.length - 1; i>=0 ; i--)
+        {
+            ans[i] *= right;
+            right *= nums[i];
+        }
+        return ans;
+    }
+}
